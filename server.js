2641613185af89a5faee741f0dfc947724e14e9e
Alice Alsford <alice.alsford@gmail.com>
2019-09-28 14:11:08 +1200

    backend wired to frontend

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const port = process.env.PORT || 3000;
 
 // app.get('/', (req, res) => res.send('Hello World!'));
 
-app.use('/', express.static('static'))
+app.use('/', express.static('static'));
+
+app.post('/api/babytracker', function (req, res) {
+    res.send('POST request to the homepage')
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
